23dd4718b50c3161767acbfae1caff268d1ee729
Naum Anteski <zcabnan@ucl.ac.uk>
2018-07-13 14:30:31 +0100

    Fixed query bugs

--- src/Utilities.java
+++ src/Utilities.java
@@ -15,11 +15,18 @@ import java.sql.ResultSet;
 public class Utilities {
     public static final Random rand = new Random();
 
-    public ArrayList<PreparedStatement> getListOfAllPreparedStatements(Connection c) throws IllegalAccessException, SQLException {
+    public  ArrayList<PreparedStatement> getListOfAllPreparedStatements(Connection c) throws IllegalAccessException, SQLException {
         List<PreparedStatement> list = new ArrayList<>(22);
         for(Field f : Queries.class.getDeclaredFields())
         {
+            if(!(f.getName().equals("sql15_view") ||f.getName().equals("sql15") || f.getName().equals("sql15_drop")) )
             list.add(c.prepareStatement((String) f.get(null)));
+        /*sql1 goes to list(0)
+        ...
+        sql 14 goes to list(13)
+        sql 16 goes to list (14)
+        sql 22 goes to list(20)
+         */
         }
         return (ArrayList<PreparedStatement>) list;
     }
@@ -31,21 +38,27 @@ public class Utilities {
             stmt22.setString(i,Integer.toString(country_codes.get(i-1)));
             stmt22.setString(i+7, Integer.toString(country_codes.get(i-1)));
         }
+        System.out.println("query 22 executed");
+
         return stmt22;
     }
 
     public PreparedStatement getPreparedStatement_21(PreparedStatement stmt21) throws SQLException {
         stmt21.setString(1, Constants.keysAsArray.get(rand.nextInt(Constants.keysAsArray.size())));
+        System.out.println("query 21 executed");
+
         return stmt21;
     }
 
     public PreparedStatement getPreparedStatement_20(PreparedStatement stmt20) throws SQLException {
-	LocalDate date = LocalDate.of(rand.nextInt(5)+1993,1,1);
-	stmt20.setString(1, Constants.colors[rand.nextInt(Constants.colors.length)]+"%");
+	    LocalDate date = LocalDate.of(rand.nextInt(5)+1993,1,1);
+	    stmt20.setString(1, Constants.colors[rand.nextInt(Constants.colors.length)]+"%");
         stmt20.setDate(2, Date.valueOf(date));
         stmt20.setDate(3,Date.valueOf(date));
         
         stmt20.setString(4, Constants.keysAsArray.get(rand.nextInt(Constants.keysAsArray.size())));
+        System.out.println("query 20 executed");
+
         return stmt20;
     }
 
@@ -62,19 +75,24 @@ public class Utilities {
         stmt19.setString(7,"BRAND#"+Integer.toString(rand.nextInt(5)+1)+Integer.toString(rand.nextInt(5)+1));
         stmt19.setInt(8,quantity3);
         stmt19.setInt(9,quantity3);
+        System.out.println("query 19 executed");
+
         return stmt19;
     }
 
     public PreparedStatement getPreparedStatement_18(PreparedStatement stmt18) throws SQLException {
         stmt18.setInt(1, rand.nextInt(4)+312);
+        System.out.println("query 18 executed");
+
         return stmt18;
     }
 
     public PreparedStatement getPreparedStatement_17(PreparedStatement stmt17) throws SQLException {
-        //stmt17.setString(1,"BRAND#"+Integer.toString(rand.nextInt(5)+1)+Integer.toString(rand.nextInt(5)+1));
-        //stmt17.setString(2, Constants.container_1[rand.nextInt(Constants.container_1.length)] + " " + Constants.container_2[rand.nextInt(Constants.container_2.length)]);
-	stmt17.setString(1,"BRAND#12");
-	stmt17.setString(2,"MED PACK");
+        stmt17.setString(1,"BRAND#"+Integer.toString(rand.nextInt(5)+1)+Integer.toString(rand.nextInt(5)+1));
+        stmt17.setString(2, Constants.container_1[rand.nextInt(Constants.container_1.length)] + " " + Constants.container_2[rand.nextInt(Constants.container_2.length)]);
+
+        System.out.println("query 17 executed");
+
         return stmt17;
     }
 
@@ -86,41 +104,55 @@ public class Utilities {
         for(int i = 3; i <=10; i++) {
             stmt16.setInt(i, sizes.get(n)); n++;
         }
+        System.out.println("query 16 executed");
+
         return stmt16;
     }
 //FIX QUERIES ABOVE 15. CHECK THEM !!!!!
-    public void getPreparedStatement_15(Connection conn) throws SQLException {
-        
-	//ResultSet rs = stmt.executeQuery(Queries.sql15);
-	//while(rs.next()){System.out.println(rs.getString(1));}
-	
-	//stmt.execute(Queries.sql15_drop);
-        //rs.close();
-	//stmt.close();
-	
+    public static synchronized void getPreparedStatement_15(Connection conn) throws SQLException {
+        int year = Utilities.rand.nextInt(5)+1993; //for q4
+        int month;
+        if(year == 1997){ month = Utilities.rand.nextInt(10) + 1; } else {month = Utilities.rand.nextInt(12) + 1;}
+        String sql;
+        String date = String.format("%d-%02d-01", year, month);
+        sql = Queries.sql15_view.replace("?", String.format("'%s'", date));
+        conn.createStatement().execute(sql);
+        ResultSet rs = conn.createStatement().executeQuery(Queries.sql15);
+        while(rs.next()) {
+            //System.out.println(rs.getString(1));
+        }
+        conn.createStatement().execute(Queries.sql15_drop);
+        System.out.println("query 15 executed");
+
     }
 
     public PreparedStatement getPreparedStatement_14(PreparedStatement stmt14) throws SQLException {
-	LocalDate date = LocalDate.of(rand.nextInt(5)+1993,rand.nextInt(12)+1,1);
+	    LocalDate date = LocalDate.of(rand.nextInt(5)+1993,rand.nextInt(12)+1,1);
         stmt14.setDate(1, Date.valueOf(date));
         stmt14.setDate(2,Date.valueOf(date));
+        System.out.println("query 14 executed");
+
         return stmt14;
     }
 
     public PreparedStatement getPreparedStatement_13(PreparedStatement stmt13) throws SQLException {
         stmt13.setString(1, "%" + Constants.words1[rand.nextInt(Constants.words1.length)] + "%" + Constants.words2[rand.nextInt(Constants.words2.length)] + "%");
+        System.out.println("query 13 executed");
+
         return stmt13;
     }
 
     public PreparedStatement getPreparedStatement_12(PreparedStatement stmt12) throws SQLException {
         String mode1 = Constants.modes[rand.nextInt(Constants.modes.length)];
         String mode2 = Constants.modes[rand.nextInt(Constants.modes.length)];
-	LocalDate date = LocalDate.of(rand.nextInt(5)+1993,1,1);
+	    LocalDate date = LocalDate.of(rand.nextInt(5)+1993,1,1);
         while(mode1.equals(mode2)) mode2 = Constants.modes[rand.nextInt(Constants.modes.length)]; //need to be different
         stmt12.setString(1,mode1);
         stmt12.setString(2,mode2);
         stmt12.setDate(3, Date.valueOf(date));
         stmt12.setDate(4,Date.valueOf(date));
+        System.out.println("query 12 executed");
+
         return stmt12;
     }
 
@@ -128,6 +160,8 @@ public class Utilities {
         stmt11.setString(1, Constants.keysAsArray.get(rand.nextInt(Constants.keysAsArray.size())));
         stmt11.setDouble(2, 0.0001 * Constants.DB_SCALE_FACTOR);
         stmt11.setString(3, Constants.keysAsArray.get(rand.nextInt(Constants.keysAsArray.size())));
+        System.out.println("query 11 executed");
+
         return stmt11;
     }
 
@@ -137,12 +171,16 @@ public class Utilities {
         if(randomYear10 == 1993) randomMonth10 = rand.nextInt(11)+2; else if(randomYear10 == 1995) randomMonth10 = 1; else randomMonth10 = rand.nextInt(12)+1;
         stmt10.setDate(1, Date.valueOf(LocalDate.of(randomYear10, randomMonth10, 1)));
         stmt10.setDate(2, Date.valueOf(LocalDate.of(randomYear10, randomMonth10, 1)));
+        System.out.println("query 10 executed");
+
         return stmt10;
     }
 
     public PreparedStatement getPreparedStatement_9(PreparedStatement stmt9) throws SQLException {
 
         stmt9.setString(1,"%"+ Constants.colors[rand.nextInt(Constants.colors.length)] +"%");
+        System.out.println("query 9 executed");
+
         return stmt9;
     }
 
@@ -153,6 +191,8 @@ public class Utilities {
         stmt8.setString(1, nation_8);
         stmt8.setString(2, region_8);
         stmt8.setString(3, x);
+        System.out.println("query 8 executed");
+
         return stmt8;
     }
 
@@ -164,6 +204,8 @@ public class Utilities {
         stmt7.setString(2,nation2_7);
         stmt7.setString(3,nation2_7);
         stmt7.setString(4,nation1_7);
+        System.out.println("query 7 executed");
+
         return stmt7;
     }
 
@@ -177,15 +219,19 @@ public class Utilities {
         stmt6.setDouble(3, Constants.seq_q6[index_q6]);
         stmt6.setDouble(4, Constants.seq_q6[index_q6]);
         stmt6.setInt(5,quantity_q6);
+        System.out.println("query 6 executed");
+
         return stmt6;
     }
 
     public PreparedStatement getPreparedStatement_5(PreparedStatement stmt5) throws SQLException {
 
         stmt5.setString(1, Constants.region2[rand.nextInt(5)]);
-	LocalDate date = LocalDate.of(rand.nextInt(5)+1993,1,1);
+	    LocalDate date = LocalDate.of(rand.nextInt(5)+1993,1,1);
         stmt5.setDate(2, Date.valueOf(date));
         stmt5.setDate(3,Date.valueOf(date));
+        System.out.println("query 5 executed");
+
         return stmt5;
     }
 
@@ -200,6 +246,8 @@ public class Utilities {
 
         stmt4.setDate(1, newDate);
         stmt4.setDate(2, newDate);
+        System.out.println("query 4 executed");
+
         return stmt4;
     }
 
@@ -214,6 +262,8 @@ public class Utilities {
         stmt3.setString(1, Constants.segments[rand.nextInt(Constants.segments.length)]);
         stmt3.setDate(2, date);
         stmt3.setDate(3,date);
+        System.out.println("query 3 executed");
+
         return stmt3;
     }
 
@@ -222,19 +272,22 @@ public class Utilities {
 
         stmt2.setInt(1,rand.nextInt(50) + 1); // for query 2 [1-50]
         stmt2.setString(2,"%"+ Constants.type3[rand.nextInt(Constants.type3.length)]);
-	int index = rand.nextInt(Constants.region2.length);
+	    int index = rand.nextInt(Constants.region2.length);
         stmt2.setString(3, Constants.region2[index]);
-	stmt2.setString(4, Constants.region2[index]);
+	    stmt2.setString(4, Constants.region2[index]);
+        System.out.println("query 2 executed");
+
         return stmt2;
     }
 
     public PreparedStatement getPreparedStatement_1(PreparedStatement stmt1) throws SQLException {
 
         stmt1.setString(1,Integer.toString(rand.nextInt(61) + 60) + "day"); //range  = [60-120]
+        System.out.println("query 1 executed");
         return stmt1;
     }
 
-    public PreparedStatement prepareStatement(int choice, Connection c) throws SQLException {
+    public  PreparedStatement prepareStatement(int choice, Connection c) throws SQLException {
         switch (choice)
         {
             case 1 : return  c.prepareStatement(Queries.sql1);
@@ -260,7 +313,7 @@ public class Utilities {
             case 22:  return c.prepareStatement(Queries.sql22);
             default: return null;
         }}
-    public PreparedStatement setParameters(int choice, PreparedStatement p) throws SQLException {
+    public  PreparedStatement setParameters(int choice, PreparedStatement p) throws SQLException {
         switch (choice)
         {
             case 1 : return getPreparedStatement_1(p);
@@ -287,7 +340,7 @@ public class Utilities {
             default: return null;
         }
     }
-    public static List<Integer> getRandomNonRepeatingList(int length, int max, int min) {
+    public  List<Integer> getRandomNonRepeatingList(int length, int max, int min) {
         List<Integer> sizes = new ArrayList<>(length);
         while(sizes.size() < length) {
             int x = rand.nextInt(max-min+1)+min;
